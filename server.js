71650ac947bd9bab34abe5459d1ff7ecfe891ef9
ProjectWithRavi <160311097+ProjectWithRavi@users.noreply.github.com>
2025-07-20 14:08:36 +0530

    some change

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 // Routes
 app.use('/api/tasks', taskRoutes);
 
-
+console.log("hello")
 // Start server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
